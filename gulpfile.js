18ce3aec5a396ddb86fe4acd27e986a6f34a0322
dtangram <dtangram@yahoo.com>
2016-07-05 16:21:38 -0500

    Made Changes for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require("gulp"),
-	uglify = require('gulp-uglify');
-
-gulp.task("default", function() {
+	uglify = require("gulp-uglify"),
+	sass = require("gulp-ruby-sass");
+	
+//Uglify Scripts Task
+gulp.task("scripts", function() {
 	gulp.src("js/*.js")
 	.pipe(uglify())
 	.pipe(gulp.dest("minjs"));
 });
+
+//Uglify Styles Task
+gulp.task("styles", function() {
+	return sass('css/*.scss', {
+	  style: 'compressed'
+	})
+	.pipe(gulp.dest('css/'));
+});
+
+//Watch Scripts Task
+gulp.task("watch", function() {
+	gulp.watch("js/*.js", ["scripts"]);
+	gulp.watch("css/*.scss", ["styles"]);
+});
+
+gulp.task("default", ["scripts", "styles", "watch"]);
